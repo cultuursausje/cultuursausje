adeef851203a4118c112a13ad06a279a65b02095
cultuursausje <cultuursausje@gmail.com>
2026-05-16 11:55:43 +0200

    Add files via upload

---
+++ site/data/gezelschappen.ts
@@ -0,0 +1,84 @@
+import type { Gezelschap } from "@/types";
+
+export const gezelschappen: Gezelschap[] = [
+  {
+    id: "ita", naam: "Internationaal Theater Amsterdam", afkorting: "ITA",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Amsterdam", url: "https://ita.nl",
+    beschrijving: "Amsterdams ensemble onder Eline Arbo, met spraakmakende producties en internationale topregisseurs."
+  },
+  {
+    id: "theater-rotterdam", naam: "Theater Rotterdam", afkorting: "TR",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Rotterdam", url: "https://www.theaterrotterdam.nl/",
+    beschrijving: "Rotterdams gezelschap onder Alida Dors. Maakt grenzeloos theater en geeft jonge makers ruimte."
+  },
+  {
+    id: "theater-oostpool", naam: "Theater Oostpool", afkorting: "Oostpool",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Arnhem", url: "https://www.oostpool.nl/",
+    beschrijving: "Arnhems gezelschap onder Daria Bukvić. Brengt actuele, vurige verhalen vanuit diverse achtergronden."
+  },
+  {
+    id: "nite", naam: "Nationaal Interdisciplinair Theater Ensemble", afkorting: "NITE",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Groningen", url: "https://nite.nl/",
+    beschrijving: "Gronings interdisciplinair gezelschap onder Guy Weizman. Energiek totaaltheater, vaak meertalig."
+  },
+  {
+    id: "toneelgroep-maastricht", naam: "Toneelgroep Maastricht", afkorting: "TM",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Maastricht", url: "https://www.toneelgroepmaastricht.nl/",
+    beschrijving: "Maastrichts gezelschap onder Michel Sluysmans. Verbindende, herkenbare verhalen vol empathie."
+  },
+  {
+    id: "het-zuidelijk-toneel", naam: "Het Zuidelijk Toneel", afkorting: "HZT",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Tilburg", url: "https://www.hzt.nl/",
+    beschrijving: "Tilburgs gezelschap onder Sarah Moeremans. Prikkelende, eigenzinnige theatrale ontmoetingen."
+  },
+  {
+    id: "het-nationale-theater", naam: "Het Nationale Theater", afkorting: "HNT",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Den Haag", url: "https://www.hnt.nl/",
+    beschrijving: "Haags gezelschap onder Eric de Vroedt. Stelt voor hoe het niet moet en wel kan."
+  },
+  {
+    id: "theater-utrecht", naam: "Theater Utrecht", afkorting: "Theater Utrecht",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Utrecht", url: "https://www.theaterutrecht.nl/",
+    beschrijving: "Utrechts gezelschap onder Anne Breure. Spraakmakend en vernieuwend voor breed publiek."
+  },
+  {
+    id: "tryater", naam: "Tryater", afkorting: "Tryater",
+    type: "Rijksgesubsidieerd theatergezelschap", stad: "Leeuwarden", url: "https://tryater.com/",
+    beschrijving: "Leeuwardens gezelschap onder Tatiana Pratley. Meertalig theater, dicht op de huid van het publiek."
+  },
+  {
+    id: "wunderbaum", naam: "Wunderbaum", afkorting: "Wunderbaum",
+    type: "Theatercollectief", stad: "Rotterdam", url: "https://wunderbaum.nl/",
+    beschrijving: "Rotterdams collectief sinds 2001. Stukken over tijdloze actuele thema's, met internationale samenwerking."
+  },
+  {
+    id: "dood-paard", naam: "Dood Paard", afkorting: "Dood Paard",
+    type: "Theatercollectief", stad: "Amsterdam", url: "https://doodpaard.nl/",
+    beschrijving: "Amsterdams collectief sinds 1993. Eigenzinnig en open theater op basis van klassieke en moderne teksten."
+  },
+  {
+    id: "de-warme-winkel", naam: "De Warme Winkel", afkorting: "DWW",
+    type: "Theatercollectief", stad: "Amsterdam", url: "https://dewarmewinkel.nl/",
+    beschrijving: "Amsterdams collectief sinds 2003. Geestig, brutaal en intelligent theater dat zichzelf graag bevraagt."
+  },
+  {
+    id: "orkater", naam: "Orkater", afkorting: "Orkater",
+    type: "Theatercollectief (muziektheater)", stad: "Amsterdam", url: "https://orkater.nl/",
+    beschrijving: "Amsterdams muziektheatercollectief sinds 1971. ORKest + theATER van eigenzinnige makers."
+  },
+  {
+    id: "bureau-vergezicht", naam: "Bureau Vergezicht", afkorting: "Bureau Vergezicht",
+    type: "Theatercollectief", stad: "Amsterdam", url: "https://www.bureauvergezicht.nl",
+    beschrijving: "Amsterdams collectief sinds 2024. Theater over de klimaatcrisis, voor publiek én politiek."
+  },
+  {
+    id: "toneelschuur-producties", naam: "Toneelschuur Producties", afkorting: "TSP",
+    type: "Productiehuis", stad: "Haarlem", url: "https://www.toneelschuurproducties.nl/",
+    beschrijving: "Haarlems productiehuis sinds 1969. Biedt jonge theatermakers ruimte om nieuwe producties te maken."
+  },
+  {
+    id: "rightaboutnow-inc", naam: "RIGHTABOUTNOW INC.", afkorting: "RAN INC.",
+    type: "Productiehuis (interdisciplinair hiphop/urban)", stad: "Amsterdam", url: "https://rightaboutnowinc.com/",
+    beschrijving: "Amsterdams productiehuis sinds 1971. Interdisciplinair hiphop- en urban-theater met sociaal-maatschappelijke projecten."
+  }
+];
