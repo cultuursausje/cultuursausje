7f463c535cab4578130ea8dce493572f70519258
cultuursausje <cultuursausje@gmail.com>
2026-05-16 16:00:02 +0200

    Add files via upload

--- site/data/theaters.ts
+++ site/data/theaters.ts
@@ -3,66 +3,79 @@ import type { Theater } from "@/types";
 export const theaters: Theater[] = [
   {
     id: "ita", naam: "Internationaal Theater Amsterdam", afkorting: "ITA",
+    stad: "Amsterdam",
     url: "https://ita.nl",
     beschrijving: "De oude Stadsschouwburg bestaat al sinds 1638 op 3 verschillende plekken in de stad."
   },
   {
     id: "de-brakke-grond", naam: "Vlaams Cultuurhuis de Brakke Grond", afkorting: "De Brakke Grond",
+    stad: "Amsterdam",
     url: "https://www.brakkegrond.nl",
     beschrijving: "Cultuurhuis aan de Nes, geopend in 1981 na financiering door de Vlaamse overheid."
   },
   {
     id: "frascati", naam: "Frascati", afkorting: "Frascati",
+    stad: "Amsterdam",
     url: "https://www.frascatitheater.nl",
     beschrijving: "Productiehuis voor jonge makers in de Nes, sinds 1924 als feestzaal opgericht."
   },
   {
     id: "bellevue", naam: "Theater Bellevue", afkorting: "Bellevue",
+    stad: "Amsterdam",
     url: "https://www.theaterbellevue.nl",
     beschrijving: "Hét theater voor opkomende theaterauteurs, in 1840 opgericht als Sociëteit."
   },
   {
     id: "de-kleine-komedie", naam: "De Kleine Komedie", afkorting: "De Kleine Komedie",
+    stad: "Amsterdam",
     url: "https://www.dekleinekomedie.nl",
     beschrijving: "Het oudste theater voor de kleinkunst, in 1786 gebouwd als Franstalig theater."
   },
   {
     id: "delamar", naam: "DeLaMar Theater", afkorting: "DeLaMar",
+    stad: "Amsterdam",
     url: "https://www.delamar.nl",
     beschrijving: "Cultuurhuis voor de massa, in 2010 verbouwd naar plan van VandenEnde Foundation."
   },
   {
     id: "carre", naam: "Koninklijk Theater Carré", afkorting: "Carré",
+    stad: "Amsterdam",
     url: "https://carre.nl",
     beschrijving: "Voormalig wintercircus van familie Carré, het grootste theater(circus) van Amsterdam."
   },
   {
     id: "de-sloot", naam: "De Sloot", afkorting: "De Sloot",
+    stad: "Amsterdam",
     url: "https://desloot.nl",
     beschrijving: "Tijdelijke theaterbroedplaats in een voormalig metaalbewerkingsbedrijf in Sloterdijk."
   },
   {
     id: "nationale-opera-en-ballet", naam: "Nationale Opera & Ballet", afkorting: "Nationale Opera & Ballet",
+    stad: "Amsterdam",
     url: "https://www.operaballet.nl",
     beschrijving: "Muziektheater in de Stopera, in 1986 geopend in een voormalige Jodenbuurt."
   },
   {
     id: "meervaart", naam: "Meervaart", afkorting: "Meervaart",
+    stad: "Amsterdam",
     url: "https://meervaart.nl",
     beschrijving: "Cultuurcentrum in Nieuw-West sinds 1977, het grootste stadsdeel van Amsterdam."
   },
   {
     id: "bijlmer-parktheater", naam: "Bijlmer Parktheater", afkorting: "BPT",
+    stad: "Amsterdam",
     url: "https://bijlmerparktheater.nl",
     beschrijving: "Sinds 2009 in het Nelson Mandela Park: podium voor intercultureel talent."
   },
   {
     id: "podium-mozaiek", naam: "Podium Mozaïek", afkorting: "Podium Mozaïek",
+    stad: "Amsterdam",
     url: "https://www.podiummozaiek.nl",
     beschrijving: "Theater geopend in 2005 in de voormalig gereformeerde Pniëlkerk in Bos en Lommer."
   },
   {
     id: "cc-amstel", naam: "CC Amstel", afkorting: "CC Amstel",
+    stad: "Amsterdam",
     url: "https://ccamstel.nl",
     beschrijving: "Cultureel clubhuis voor podiumkunsten in de Pijp, geopend in 2019."
   }
